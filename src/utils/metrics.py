202619ccaf4be2e5e6c85f1cbf7fbe8dc599745f
Artem Ryzhikov <artemryzhikov@gmail.com>
2023-02-03 13:29:07 +0300

    Metrics updated

--- src/utils/metrics.py
+++ src/utils/metrics.py
@@ -5,17 +5,14 @@ import re
 from sklearn.utils import resample
 import pdb
 
-def absolute_percentage_error(y_true, y_pred):
-    eps = 1e-8
-    return np.abs((y_true-y_pred)/(eps+y_true))*100
-
 def _aggregate_loads(load_ids, y_true, y_pred, metric_fn, n_iters=100):
     errors = []
     load_ids = np.array(load_ids)
     for id in set(load_ids):
         load_inds = np.argwhere(id == load_ids)
         errors.append(_eval_load(y_true[load_inds], y_pred[load_inds], metric_fn, n_iters))
-    return np.mean([x[0] for x in errors]), np.sqrt(sum(map(lambda x: x[1]**2,errors)))
+    return np.mean([x[0] for x in errors]), np.sqrt(sum(map(lambda x: x[1]**2,errors)/len(errors)))
+
 
 def _eval_load(y_true, y_pred, metric_fn, n_iters):
     load_errors = []
@@ -28,13 +25,13 @@ def _eval_load(y_true, y_pred, metric_fn, n_iters):
     return np.mean(load_errors, axis=0), np.std(load_errors, axis=0)
 
 
-def mean_estimation_absolute_percentage_error(load_ids, y_true, y_pred, n_iters=100):   
-    def metric_fn(y_true, y_pred, eps=1e-8):
+def mean_estimation_absolute_percentage_error(load_ids, y_true, y_pred, n_iters=100, eps=1e-8):   
+    def metric_fn(y_true, y_pred, eps=eps):
         return np.abs((y_true.mean(0)-y_pred.mean(0))/(eps+y_true.mean(0))).squeeze()*100
     return _aggregate_loads(load_ids, y_true, y_pred, metric_fn, n_iters)    
 
 def std_estimation_absolute_percentage_error(load_ids, y_true, y_pred, n_iters=100, eps=1e-8):
-    def metric_fn(y_true, y_pred, eps=1e-8):
+    def metric_fn(y_true, y_pred, eps=eps):
         return np.abs((y_true.std(0)-y_pred.std(0))/(eps+y_true.std(0))).squeeze()*100
     return _aggregate_loads(load_ids, y_true, y_pred, metric_fn, n_iters)    
 
