c6b25240be1dc59c6784f386009da2fd744682ea
Aziz Rashid <abdalaziz.rashid@outlook.com>
2023-03-03 02:24:44 +0000

    Update

--- scripts/calculate_summary_stats.py
+++ scripts/calculate_summary_stats.py
@@ -1,50 +1,175 @@
+import argparse
+from pathlib import Path
+import logging
+import json
+import numpy as np
+import pandas as pd
 from digital_twin.performance_metrics import ape, meape, seape, aggregate_loads
-from digital_twin.performance_metrics import mmd_rbf, frechet_distance  as fd
+from digital_twin.performance_metrics import mmd_rbf, frechet_distance as fd
 from digital_twin.visulization.plots import Figures
-meape_iops_mean, meape_iops_std = aggregate_loads(
-        ids[ids.isin(test_ids)].values, y_test.values[:, 0], y_pred[:, 0], meape
-    )
-    meape_lat_mean, meape_lat_std = aggregate_loads(
-        ids[ids.isin(test_ids)].values, y_test.values[:, 1], y_pred[:, 1], meape
-    )
+from tqdm import tqdm
+import uuid
 
-    seape_iops_mean, seape_iops_std = aggregate_loads(
-        ids[ids.isin(test_ids)].values, y_test.values[:, 0], y_pred[:, 0], seape
-    )
-    seape_lat_mean, seape_lat_std = aggregate_loads(
-        ids[ids.isin(test_ids)].values, y_test.values[:, 1], y_pred[:, 1], seape
-    )
+NICE_NAMES = {
+    "block_size": "Block size",
+    "n_jobs": "Number of jobs",
+    "iodepth": "IO depth",
+    "read_fraction": "Read fraction",
+    "load_type": "Load type",
+    "io_type": "IO type",
+    "raid": "RAID",
+    "n_disks": "Number of disks",
+    "device_type": "Device type",
+    "offset": "Offset",
+    "id": "ID",
+    "sequential": "Sequential",
+    "read": "Read",
+    "rand": "Rand",
+    "random": "Random",
+    "ssd": "SSD",
+    "hdd": "HDD",
+}
 
-    with open(model_checkpoint_path, "wb") as f:
-        pickle.dump(model, f)
-    
+logger = logging.getLogger(__name__)
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+# save logs to file and console
+if not Path("logs").exists():
+    Path("logs").mkdir(parents=True)
+logger.addHandler(logging.FileHandler("results/summary.log"))
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--results", type=Path, default="results/predictions")
+    return parser.parse_args()
+
+
+def transpose(l):
+    return list(map(list, zip(*l)))
+
+
+def nice_format(x: dict):
+    return {k: f"{mean:.2f} ± {std:.2f}" for k, (mean, std) in x.items()}
+
+
+def metrics_eval(target: np.array, prediction: np.array):
+    target_iops, target_lat, gen_iops, gen_lat = (
+        target[:, 0],
+        target[:, 1],
+        prediction[:, 0],
+        prediction[:, 1],
+    )
     return {
-        "MEAPE_IOPS": {"mean": meape_iops_mean, "std": meape_iops_std},
-        "MEAPE_LAT": {"mean": meape_lat_mean, "std": meape_lat_std},
-        "SEAPE_IOPS": {"mean": seape_iops_mean, "std": seape_iops_std},
-        "SEAPE_LAT": {"mean": seape_lat_mean, "std": seape_lat_std},
+        "MMD (RBF)": mmd_rbf(target, prediction),
+        "FD": fd(target, prediction),
+        "MEAPE_IOPS": meape(target_iops, gen_iops),
+        "MEAPE_LAT": meape(target_lat, gen_lat),
+        "SEAPE_IOPS": seape(target_iops, gen_iops),
+        "SEAPE_LAT": seape(target_lat, gen_lat),
     }
-   
-   def metrics_eval(target: np.array, prediction: np.array):
-    metrics = {"mmd_rbf": mmd_rbf(target, prediction), "FD": fd(target, prediction)}
+
+
+def caclulate_stats(df, conds, title=None, save_path=None):
+    metrics = metrics_eval(
+        df[["iops", "lat"]].values, df[["gen_iops", "gen_lat"]].values
+    )
+    plotter = Figures(df, filter_outliers=False)
+    fig = plotter.plot_iops_latency(title=title, conds=conds, save_path=save_path)
     return metrics
 
 
-def plot_figures(df, grouped_indices):
-    for i, idx in enumerate(grouped_indices):
-        sub_df = df.loc[idx][POOL_INDEPENDENT_VARS].drop_duplicates().to_dict('record')
-        assert len(sub_df) == 1, "More than one unique combination of independent variables"
-        title = ", ".join([f'{k}={v}' for k, v in sub_df[0].items()])
-        pgf = Figures(df.loc[idx])
-        pgf.plot_iops_latency(
-            title,
-            save_path=f"figures/{title}.png",
-            save_kwargs={"dpi": 300},
-        ) 
+def get_aggregated_stats(dfs):
+    metrics = {}
+    for df, file in tqdm(zip(dfs, files)):
+        metrics[file.stem] = caclulate_stats(
+            df,
+            conds=None,
+            title=file.stem,
+            save_path=file.parent.parent.parent
+            / "figures"
+            / file.parent.name
+            / f"agg_{file.stem}.pdf",
+        )
+    metrics = pd.DataFrame.from_records(metrics).T
+    metrics.to_csv("results/agg_metrics.csv", index=False)
 
-metrics = metrics_eval(
-        test_set_predictions[["iops", "lat"]].values,
-        test_set_predictions[["generated_iops", "generated_lat"]].values,
+
+def get_name_from_cond(cond: dict):
+    return {NICE_NAMES.get(k, k): NICE_NAMES.get(v, str(v)) for k, v in cond.items()}
+
+
+def agg(x):
+    t_x = transpose(x)
+    return np.mean(x[0]), np.mean(x[1])
+
+
+def main(files: list[Path]):
+    dfs = [pd.read_csv(f) for f in files]
+    # TODO: figure out the grouping
+    # >>> list(map(len, groupded_dfs))
+    # [16, 32, 16, 16, 32, 16, 32, 32]
+    # >>> list(map(lambda x: len(x.groupby('id')), dfs))
+    # [16, 16, 16, 16, 16, 16, 16, 16]
+
+    # groupded_dfs = [
+    #     df.groupby(
+    #         columns := df.drop(
+    #             ["iops", "lat", "gen_iops", "gen_lat"], axis=1
+    #         ).columns.tolist()
+    #     )
+    #     for df in dfs
+    # ]
+    columns = (
+        dfs[0].drop(["iops", "lat", "gen_iops", "gen_lat"], axis=1).columns.tolist()
     )
+    groupded_dfs = [df.groupby("id") for df in dfs]
+
+    # get_aggregated_stats(dfs)
+    for grouped_df, file in zip(groupded_dfs, files):
+        results = []
+        for g, df in grouped_df:
+            # TODO this is not be needed when grouping by conditions
+            g = df.loc[:, columns].drop_duplicates().values.flatten().tolist()
+            conditions = get_name_from_cond(dict(zip(columns, g)))
+            this_uuid = uuid.uuid5(
+                uuid.NAMESPACE_OID, json.dumps(conditions, sort_keys=True)
+            )
+            metrics = caclulate_stats(
+                df,
+                conditions,
+                title=file.stem,
+                save_path=file.parent.parent.parent
+                / "figures"
+                / file.parent.name
+                / file.stem
+                / f"{this_uuid}.pdf",
+            )
+            results.append(dict(UUID=str(this_uuid), **metrics, **conditions))
+        this_group_df = pd.DataFrame.from_dict(results)
+        root_path = file.parent.parent.parent / "metrics" / file.parent.name
+        root_path.mkdir(parents=True, exist_ok=True)
+        this_group_df.to_csv(root_path / f"metrics_{file.stem}.csv", index=False)
+        this_group_df.agg(
+            {
+                "MMD (RBF)": agg,
+                "FD": agg,
+                "MEAPE_IOPS": agg,
+                "MEAPE_LAT": agg,
+                "SEAPE_IOPS": agg,
+                "SEAPE_LAT": agg,
+            },
+        ).to_csv(
+            root_path / f"metrics_agg_{file.stem}.csv",
+            index=True,
+            header=["(mean, std)"],
+        )
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    files = list(args.results.rglob("*.csv"))
+    metrics = main(files)
     logger.info(metrics)
-    plot_figures(test_set_predictions, test_set_indices)
