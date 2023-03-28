22b8ea5b800655d13bd2f53e4811bb3b1d7506c0
Artem Ryzhikov <artemryzhikov@gmail.com>
2023-03-28 13:37:39 +0300

    Columns transformer added

--- digital_twin/models/density_estimation/knn.py
+++ digital_twin/models/density_estimation/knn.py
@@ -4,7 +4,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.metrics import make_scorer
 from sklearn.model_selection import GridSearchCV
 from digital_twin.performance_metrics.misc import absolute_percentage_error as ape
-
+from sklearn.base import BaseEstimator, TransformerMixin
 
 import numpy as np
 import pandas as pd
@@ -15,9 +15,26 @@ def default_scoring_fn(y_true, y_pred):
     y_pred = scaler.transform(y_pred)
     return -ape(y_true, y_pred).mean()
 
+class YadroColumnsTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self, columns_to_drop=['device_type']):
+        self.columns_to_drop = columns_to_drop
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        X = X[[col for col in X.columns if col not in self.columns_to_drop]]
+        X['raid_0'] = X.raid.apply(lambda x: x.split('+')[0]).astype(int)
+        X['raid_1'] = X.raid.apply(lambda x: x.split('+')[1]).astype(int)
+        if y is None:
+            return X.drop(['raid'], 1)
+        return X.drop(['raid'], 1), y
 
 class KNNSampler:
-    def __init__(self, scoring_fn=default_scoring_fn, categories=['load_type', 'io_type', 'device_type'], params_grid={'n_neighbors': [1, 2, 5, 7, 10, 15, 20], 'p': [1, 2]}):
+    def __init__(self, scoring_fn=default_scoring_fn, columns_to_drop=['device_type'],
+                 categories=['load_type', 'io_type', 'device_type'], params_grid={'n_neighbors': [1], 'p': [2]}):
+                 #params_grid={'n_neighbors': [1, 2, 5, 7, 10, 15, 20], 'p': [1, 2]}):
+        self.column_transformer = YadroColumnsTransformer(columns_to_drop)
         self.model = Pipeline([
             ('encoding', OneHotEncoder(sparse=False, handle_unknown='infrequent_if_exist')),
             ('scaling', StandardScaler()),
@@ -26,11 +43,14 @@ class KNNSampler:
                                scoring=make_scorer(scoring_fn)))
         ])
 
+
     def fit(self, X: pd.DataFrame, y: pd.DataFrame):
-        self._features = list(X.columns)
+        self._orig_features = list(X.columns)
+        X = self.column_transformer.transform(X)
+        self._transformed_features = list(X.columns)
         self._targets = list(y.columns)
-        df_grouped = pd.concat([X, y], axis=1).groupby(self._features).apply(lambda df: df.mean())[self._targets].reset_index(drop=False)
-        X_grouped, y_grouped = df_grouped[self._features], df_grouped[self._targets]
+        df_grouped = pd.concat([X, y], axis=1).groupby(self._transformed_features).apply(lambda df: df.mean())[self._targets].reset_index(drop=False)
+        X_grouped, y_grouped = df_grouped[self._transformed_features], df_grouped[self._targets]
         self._X = X
         self._y = y
         self._X_grouped = X_grouped
@@ -38,9 +58,10 @@ class KNNSampler:
         self.model.fit(X_grouped, y_grouped)
 
     def sample(self, n_samples:int, **configuration):
-        X_test = np.array([configuration[k] for k in self._features])
+        X_test = np.array([configuration[k] for k in self._orig_features])
         X_test = np.expand_dims(X_test, 0)
-        X_test_transformed = pd.DataFrame(X_test, columns=self._features).transform(pd.to_numeric, errors='ignore', downcast='float')
+        X_test_transformed = pd.DataFrame(X_test, columns=self._orig_features).transform(pd.to_numeric, errors='ignore', downcast='float')
+        X_test_transformed = self.column_transformer.transform(X_test_transformed)
         if isinstance(self.model, Pipeline):
             for name, transform in self.model.steps[:-1]:
                 X_test_transformed = transform.transform(X_test_transformed)
@@ -54,9 +75,11 @@ class KNNSampler:
         
         for _, row in neigh_df.iterrows():
             samples_df = df
-            for feat in self._features:
+            for feat in self._transformed_features:
                 samples_df = samples_df[samples_df[feat] == row[feat]]
             samples_dfs.append(samples_df)
         samples_df = pd.concat(samples_dfs).sample(n_samples).reset_index(drop=True)
+        samples_df['raid'] = samples_df['raid_0'].astype(str) + '+' + samples_df['raid_1'].astype(str)
+        samples_df.drop(['raid_0', 'raid_1'], 1, inplace=True)
         return samples_df
 #         return samples_df[self._features], samples_df[self._targets]
