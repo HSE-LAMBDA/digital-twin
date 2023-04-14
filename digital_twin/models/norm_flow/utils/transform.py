5ada0420e53805642ef0bb02c9648d993f26f8fd
hushchyn-mikhail <hushchyn.mikhail@gmail.com>
2023-04-15 00:42:31 +0300

    minor updates

--- digital_twin/models/norm_flow/utils/transform.py
+++ digital_twin/models/norm_flow/utils/transform.py
@@ -3,9 +3,10 @@ import numpy as np
 from dataclasses import dataclass
 
 class LogScaler(TransformerMixin, BaseEstimator):
-    def __init__(self, eps=1e-8):
+    def __init__(self, eps=1e-8, a_max=50):
         super().__init__()
         self.eps = eps
+        self.a_max = a_max
         
     def fit(self, X, y=None):
         return self
@@ -14,4 +15,5 @@ class LogScaler(TransformerMixin, BaseEstimator):
         return np.log(X+self.eps)
     
     def inverse_transform(self, X):
+        X = np.clip(X, a_min=None, a_max=self.a_max)
         return np.exp(X)-self.eps
