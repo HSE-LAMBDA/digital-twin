b1a825ac6d349513d0ab586f8f6db6d4ea1e9c65
hushchyn-mikhail <hushchyn.mikhail@gmail.com>
2023-04-20 10:30:47 +0300

    train and test matching

--- scripts/denesity_estimation_experiment.py
+++ scripts/denesity_estimation_experiment.py
@@ -47,6 +47,18 @@ def parse_args():
     return args
 
 
+def match_files(train_files, test_files):
+    train_files_new = []
+    test_files_new = []
+    for atrain in train_files:
+        train_files_new.append(atrain)
+        for atest in test_files:
+            if str(atrain).split("train_")[-1] == str(atest).split("test_")[-1]:
+                test_files_new.append(atest)
+                break
+    return train_files_new, test_files_new
+
+
 def get_X_y(df):
     return df.drop(["iops", "lat", "id"], axis=1), df[["iops", "lat"]]
 
@@ -125,6 +137,7 @@ def get_predictions(
 
 
 def main(train_file, test_file):
+    print("Train: %s \nTest: %s \n" % (train_file, test_file))
     train_df = pd.read_csv(train_file)
     test_df = pd.read_csv(test_file)
 
@@ -151,5 +164,6 @@ if __name__ == "__main__":
     args = parse_args()
     train_files = list(args.data.rglob("train_*.csv")) 
     test_files = list(args.data.rglob("test_*.csv"))
+    train_files, test_files = match_files(train_files, test_files)
 
     process_map(main, train_files, test_files)
